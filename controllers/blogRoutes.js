4c9d0a8bf09d0e80da94286835db7bfcdbf65359
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-02-15 21:02:50 -0500

    fixed blog routes

--- controllers/blogRoutes.js
+++ controllers/blogRoutes.js
@@ -12,7 +12,7 @@ const http= require ("http");
 const util = require("util");
 
 
-// http://localhost:3001/api/blog/api
+// get all blog posts --- works!
 router.get('/', async (req,res)=> {
   try { 
       const blogData = await Blog.findAll()
@@ -30,99 +30,28 @@ router.get('/', async (req,res)=> {
   });  
 
 
-// // Getting main handlebar file
-// router.get('/blog', withAuth, async (request,response)=> {
-//  try { response.sendFile(path.join(__dirname, "views", "main.handlebars"));
-// console.log("retrieving index file");
-//  } catch (err) {
-//   res.status(500).json(err);
-// }
-// });  
+// Create new blog post
 
-// // Getting existing notes from Json file
-// router.get('/seeds/blogData', (request,response)=> {
-//   fs.readFile(path.join(__dirname, "seed","blogData.json"),"utf8", (err, jsonString) => {
-//       if (err){
-//           console.log ("Cannot read file:", err)
-//       }
-
-//       console.log ('File data:', jsonString);
-//       response.json(JSON.parse(jsonString));
-//   });
-
-
-// });
-
-
-// //  Use the post method to collect a new note and save it to the html page body
-
-// router.post('/seeds/blogData', (request,response) => {
-//     fs.readFile(path.join(__dirname, "seed", "blogData.json"), 'utf8', (err,jsonString)=> {
-//         if (err){ console.log ("Could not read file:", err)
-//         return
-//         }
-//         console.log('File data:', jsonString);
-
-//         var notes = JSON.parse(jsonString);
-    
-
-//         // object for notes
-//         const newNote = {
-//             title: request.body.title,
-//             text: request.body.text,
-//             id: Math.random().toString(36).substring(2,9)
-//         };
-
-//         notes.push(newNote);
-//         let NotesJSON = JSON.stringify(notes);
-
-//     fs.writeFile(path.join(__dirname,"seed", "blogData.json"), NotesJSON, (err)=>
-//     { if (err){
-//         console.log (err);
-//     }
-//     console.log ("Your Note was successfully added!", NotesJSON);
-//     return NotesJSON;
-
-//     });
-// });
-// });
+  router.post('/', async (req, res) => {
+    try {
+      const blogData = await Blog.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(blogData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 
 
-// // Use the delete method to remove exisitng notes
-// router.delete(`/seeds/blogdata/:id`, (request, response)=> {
-//   fs.readFile(path.join(__dirname,"seed", "blogData.json"), 'utf8',(err,jsonString)=>{
-//       if (err){
-//           console.log("Could not read file:", err)
-//           return
-//       }
+// Delete one Blog post
 
-//       console.log('File data:', jsonString);
 
-//       var notes= JSON.parse(jsonString);
 
-//   const newNote = {
-//       title: request.body.title,
-//       text: request.body.text,
-//       id: uuidv4()
 
-//   };
-
-//   notes.splice(request.params.id,1);
-
-//   let NotesJSON = JSON.stringify(notes);
-
-//   fs.writeFile(path.join(__dirname, "views", "blogData.json"), NotesJSON, (err)=>
-//   {
-//       if (err) {
-//           return console.log("could not delete this note:", err)
-//       }
-//       console.log("Note has successfully been deleted!", NotesJSON);
-//       return NotesJSON;
-//   });
-//   });
-
-// });
 
 
 module.exports = router;
