b09a6110eb5d298117825206dab9a5c7e63f3360
Silvia Trejo <s.y.trejo26@gmail.com>
2022-02-15 23:35:48 -0500

    troubleshooting files

--- controllers/regForm.js
+++ controllers/regForm.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const passport = require('passport');
-const { User } = require('../models/User');
+const { User } = require('../models');
 
-
+// deleted the end of the require chain for User
 //Route to the Register Form from the Login Page
 
 router.get('/', async (req, res) => {
