b6327b0c77239aa3faf488d7c6a9eedda44d3d1c
Silvia Trejo <s.y.trejo26@gmail.com>
2022-02-18 20:45:40 -0500

    updating roots

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('../blogRoutes');
+const workoutsRoutes = require('../workouts');
 
 
 
 
 router.use('/users', userRoutes);
 router.use('/blog', blogRoutes);
-
+router.use('/workouts', workoutsRoutes);
 
 
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,25 @@
+
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 const blogRoutes = require("./blogRoutes");
 const newRegForm = require('./regForm');
 const newUserReg = require('./newUser');
+const workouts = require('./workouts')
+
+
+
 
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-router.use("/api/blog", blogRoutes);
+// router.use("/api/blog", blogRoutes);
 router.use('/register', newRegForm);
 router.use('/register/login', newUserReg);
 router.use('/api/workouts', workouts)
 
 module.exports = router;
+
+// changed the file name from newUserRegistration to regForm because it takes me to the registration form
+// however, I need to build a newUserRegistration route-
 
